ec93a7f8cc52090b16c4b90b07c1ecfcf6a9f85d
Bruno Risso <brunorisso58@gmail.com>
2023-02-17 20:30:36 -0300

    Desafio Jokenpo JavaScript

---
+++ 10022023/jokenpo.js
@@ -0,0 +1,23 @@
+let option1 = prompt("Jogador 1\nEscolha entre as opções: \n1- Pedra\n2- Papel\n3- Tesoura");
+let option2 = prompt("Jogador 2\nEscolha entre as opções: \n1- Pedra\n2- Papel\n3- Tesoura");
+
+const verifyWinner = (option1, option2) => {
+  let winner = "";
+  if (option1 == 1) {
+    option2 == 2 ? winner = 2 : "";
+    option2 == 3 ? winner = 1 : "";
+  } else if (option1 == 2) {
+    option2 == 1 ? winner = 1 : "";
+    option2 == 3 ? winner = 2 : "";
+  } else if (option1 == 3) {
+    option2 == 1 ? winner = 2 : "";
+    option2 == 2 ? winner = 1 : "";
+  }
+  if (winner == "") {
+    return `Empate!`;
+  } else {
+    return `Vencedor: jogador ${winner}`;
+  }
+}
+
+alert(verifyWinner(option1, option2));
